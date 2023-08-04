540e3e9526894cafda5d44607fc38b40a1e86470
Ali Noor <ali.noor12ny@gmail.com>
2023-08-03 20:47:23 -0400

    allure report done-7

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -1,14 +1,18 @@
 package com.qa.opencart.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.exception.FrameworkException;
@@ -84,17 +88,17 @@ public class DriverFactory {
 
 	}
 
-//	public static String getScreenshot() {
-//		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-//		String path = System.getProperty("user.dir") + "\\screenshot.png";
-//		// String path = System.getProperty("user.dir") + "\\screenshot\\" +
-//		// System.currentTimeMillis() + ".png";
-//		File destination = new File(path);
-//		try {
-//			FileHandler.copy(srcFile, destination);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return path;
-//	}
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		// String path = System.getProperty("user.dir") + "\\screenshot.png";
+		String path = System.getProperty("C:\\Users\\user\\eclipse-workspace\\Nov2022POMSeries\\screenshot\\"
+				+ System.currentTimeMillis() + ".png");
+		File destination = new File(path);
+		try {
+			FileHandler.copy(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
+	}
 }
