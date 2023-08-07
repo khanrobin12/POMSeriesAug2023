50e5f499ae1ed99a8377ceb97c2e59ca14c4f706
Ali Noor <ali.noor12ny@gmail.com>
2023-08-07 15:44:28 -0400

    multiple test

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
 	public static final int DEFAULT_MEDIUM_TIME_OUT = 10;
 	public static final int DEFAULT_LONG_TIME_OUT = 15;
 
-	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login";
+	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login1";
 	public static final String ACCOUNT_PAGE_TITLE_VALUE = "My Account";
 
 	public static final String LOGIN_PAGE_URL_FRACTION_VALUE = "route=account/login";
